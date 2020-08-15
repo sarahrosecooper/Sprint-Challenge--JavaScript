369cb7f1c50e8d6499500e2fb2ffd3cb359865a0
sarah rose <xsarahrosemeow@gmail.com>
2020-08-14 21:56:59 -0400

    finished classes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -1,57 +1,57 @@
-/* ===== Prototype Practice ===== */
+// /* ===== Prototype Practice ===== */
 
-// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+// // Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
 
-/* == Step 1: Base Constructor ==
-  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
-*/
+// /* == Step 1: Base Constructor ==
+//   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+// */
 
-function CuboidMaker(length, width, height) {
-  this.length = length;
-  this.width = width;
-  this.height = height;
-}
+// function CuboidMaker(length, width, height) {
+//   this.length = length;
+//   this.width = width;
+//   this.height = height;
+// }
 
-/* == Step 2: Volume Method ==
-  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
-  
-  Formula for cuboid volume: length * width * height
-*/
+// /* == Step 2: Volume Method ==
+//   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
 
-CuboidMaker.prototype.volume = function () {
-  return this.length * this.width * this.height;
-};
+//   Formula for cuboid volume: length * width * height
+// */
 
-/* == Step 3: Surface Area Method ==
-  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
+// CuboidMaker.prototype.volume = function () {
+//   return this.length * this.width * this.height;
+// };
 
-  Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
-*/
+// /* == Step 3: Surface Area Method ==
+//   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height.
 
-CuboidMaker.prototype.surfaceArea = function () {
-  return (
-    2 *
-    (this.length * this.width +
-      this.length * this.height +
-      this.width * this.height)
-  );
-};
+//   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
+// */
 
-/* == Step 4: Create a new object that uses CuboidMaker ==
-  Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
-  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
-*/
+// CuboidMaker.prototype.surfaceArea = function () {
+//   return (
+//     2 *
+//     (this.length * this.width +
+//       this.length * this.height +
+//       this.width * this.height)
+//   );
+// };
 
-const cuboid = new CuboidMaker(4, 5, 5);
+// /* == Step 4: Create a new object that uses CuboidMaker ==
+//   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
+//   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.
+// */
 
-console.log(cuboid);
+// let cuboid = new CuboidMaker(4, 5, 5);
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(
-  "the answer to prototypes step 4 is: the volume is:",
-  cuboid.volume()
-); // 100
-console.log(
-  "the answer to prototypes step 4 is: the surface is:",
-  cuboid.surfaceArea()
-); // 130
+// console.log(cuboid);
+
+// // Test your volume and surfaceArea methods by uncommenting the logs below:
+// console.log(
+//   "the answer to prototypes step 4 is: the volume is:",
+//   cuboid.volume()
+// ); // 100
+// console.log(
+//   "the answer to prototypes step 4 is: the surface is:",
+//   cuboid.surfaceArea()
+// ); // 130
